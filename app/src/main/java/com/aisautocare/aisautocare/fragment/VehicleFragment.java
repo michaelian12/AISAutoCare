a0e7b88906d5ccf055c0c01ad3e9048fdc4bb3dd
Michael Agustian <michaelian12@gmail.com>
2017-01-06 21:42:19 +0700

    Adding floating action button

--- app/src/main/java/com/aisautocare/aisautocare/fragment/VehicleFragment.java
+++ app/src/main/java/com/aisautocare/aisautocare/fragment/VehicleFragment.java
@@ -1,5 +1,6 @@
 package com.aisautocare.aisautocare.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -26,6 +27,7 @@ public class VehicleFragment extends Fragment {
     private View rootView;
     private RecyclerView recyclerView;
     private VehicleRecyclerViewAdapter adapter;
+    private FloatingActionButton fab;
 
     public VehicleFragment() {
         // Required empty public constructor
@@ -35,6 +37,18 @@ public class VehicleFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_vehicle, container, false);
         recyclerView = (RecyclerView) rootView.findViewById(R.id.recycler_view);
+
+        // Floating action button
+        fab = (FloatingActionButton) rootView.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+//                Intent intent = new Intent(MainActivity.this, NewMessageActivity.class);
+//                startActivity(intent);
+            }
+        });
 
         // Create a list of vehicles
         ArrayList<Vehicle> vehicles = new ArrayList<Vehicle>();
