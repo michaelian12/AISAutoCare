cebf383b441f7219ea62b0059a7a0cf1edea3ce4
Michael Agustian <michaelian12@gmail.com>
2017-01-21 00:44:44 +0700

    fixing firebase and maps api

---
+++ app/src/main/java/com/aisautocare/mobile/activity/OrderActivity.java
@@ -0,0 +1,69 @@
+package com.aisautocare.mobile.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
+
+import info.androidhive.firebasenotifications.R;
+
+public class OrderActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    LinearLayout addressLayout;
+    TextView addressTextView;
+    // konstanta untuk mendeteksi hasil balikan dari place picker
+    private int PLACE_PICKER_REQUEST = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+        /* Set Toolbar */
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        addressTextView = (TextView) findViewById(R.id.address_order_text_view);
+
+        addressLayout = (LinearLayout) findViewById(R.id.address_order_layout);
+        addressLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+                try {
+                    //menjalankan place picker
+                    startActivityForResult(builder.build(OrderActivity.this), PLACE_PICKER_REQUEST);
+
+                    // check apabila <a title="Solusi Tidak Bisa Download Google Play Services di Android" href="http://www.twoh.co/2014/11/solusi-tidak-bisa-download-google-play-services-di-android/" target="_blank">Google Play Services tidak terinstall</a> di HP
+                } catch (GooglePlayServicesRepairableException e) {
+                    e.printStackTrace();
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // menangkap hasil balikan dari Place Picker, dan menampilkannya pada TextView
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(data, this);
+                String toastMsg = String.format(
+                        "Place: %s \n" + "Alamat: %s \n", place.getName(), place.getAddress());
+                addressTextView.setText(toastMsg);
+            }
+        }
+    }
+}
